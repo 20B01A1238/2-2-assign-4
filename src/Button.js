b3acf215c10417a2c4f49fb6db2d8e120e3e3425
Lavanya Sai Priya <harshithasaipriya2003@gmail.com>
2022-02-13 12:04:55 +0530

    assign3

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+const Button=()=>{
+    return(
+        <div className="extra content">
+      <div className="ui two buttons">
+        <div className="ui basic green button">Approve</div>
+        <div className="ui basic red button">Decline</div>
+      </div>
+    </div>
+    )
+  }
+  export default Button;
